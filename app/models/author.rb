957a2575fb1d59729a401652caa5dee09cf713c7
Lee Chun Hoe <leechunhoe0625@gmail.com>
2018-01-13 20:57:49 +0800

    Use encode safe hong im; Delete all before import artists; Do artists layout

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,8 +3,8 @@ require 'csv'
 
 class Author < ApplicationRecord
 	def self.import_from_file
+		Author.delete_all
 		artists = CSV.read("artists.csv", { :col_sep => "\t", :headers => true})
-
 		artists.each do |artist|
 			# puts artist.inspect
 			Author.create!(artist.to_hash)
